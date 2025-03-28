9cb0963d330f4414cff113c8d6a82ef9787913ae
Ronesh Bhandari <homepc@Roneshs-MacBook-Pro.local>
2025-03-27 23:42:46 -0500

    Changes

--- PythonBasics/SalesTax.py
+++ PythonBasics/SalesTax.py
@@ -2,3 +2,11 @@ amount = float(input("What's the price : "))
 salestax = 0.06
 finalprice = amount + (amount*salestax)
 print("Your final price is $", finalprice, sep='')
+
+
+salrb = 140000
+salpc = 205000
+
+salsum = (salrb + salpc)
+tax = salsum * 0.15
+print("Your IT is $", tax)
